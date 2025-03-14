d5c311a8c43dd35969e8782bff373915b1257a91
Alejandro-Vega <vegaalejandro15@gmail.com>
2025-03-14 15:30:57 -0400

    Upcart cart link styling

--- src/components/NavBar/CartLink.js
+++ src/components/NavBar/CartLink.js
@@ -1,19 +1,15 @@
-import React from "react";
-import { Box, Tooltip, withStyles } from "@material-ui/core";
+import React from 'react';
+import { Box, Tooltip, withStyles } from '@material-ui/core';
 import { NavLink } from 'react-router-dom';
 
 const CartLink = ({ navBarCartData, numberOfCases, classes }) => {
   const getCartLabel = (labelType) => {
     return (
       <div className={classes.cartCounter2Wrapper}>
-      <div className={classes.cartCounter2}>
-        {numberOfCases}
+        <div className={classes.cartCounter2}>{numberOfCases}</div>
+        <div className={classes.cartLabel}>Files</div>
       </div>
-      <div className={classes.cartLabel}>
-        Files
-      </div>
-    </div>
-    )
+    );
   };
 
   return (
@@ -24,7 +20,6 @@ const CartLink = ({ navBarCartData, numberOfCases, classes }) => {
     >
       <NavLink className={classes.cartLabelText} to={navBarCartData.cartLink}>
         {navBarCartData.cartLabel}
-        {/* <Badge badgeContent={numberOfCases} max={99999}> */}
         <Tooltip title="Files" placement="bottom-end">
           <span className={classes.badge}>
             <img
@@ -53,31 +48,49 @@ const styles = () => ({
     fontWeight: '700',
     letterSpacing: '0.08rem',
     textAlign: 'start',
-    fontSize: '10px',
+    fontSize: '12px',
   },
   cartLabel: (props) => ({
-    color: props.navBarstyling && props.navBarstyling.cartLabel && props.navBarstyling.cartLabel.color ? props.navBarstyling.cartLabel.color : '#E37B22',
+    color:
+      props.navBarstyling &&
+      props.navBarstyling.cartLabel &&
+      props.navBarstyling.cartLabel.color
+        ? props.navBarstyling.cartLabel.color
+        : '#E37B22',
     lineHeight: '13px',
     minWidth: '16px',
     fontFamily: 'Poppins',
     fontWeight: '700',
     letterSpacing: '0.08rem',
     textAlign: 'start',
-    fontSize: '10px',
+    fontSize: '12px',
   }),
   logotype: (props) => ({
     whiteSpace: 'nowrap',
     color: '#FFFFFF',
-    fontFamily: props.navBarstyling && props.navBarstyling.global.fontFamily ? props.navBarstyling.global.fontFamily : 'Raleway',
+    fontFamily:
+      props.navBarstyling && props.navBarstyling.global.fontFamily
+        ? props.navBarstyling.global.fontFamily
+        : 'Raleway',
     fontSize: '11px',
-    letterSpacing: props.navBarstyling && props.navBarstyling.global.letterSpacing ? props.navBarstyling.global.letterSpacing : '1.25px',
-    fontWeight: props.navBarstyling && props.navBarstyling.global.fontWeight ? props.navBarstyling.global.fontWeight : '800',
+    letterSpacing:
+      props.navBarstyling && props.navBarstyling.global.letterSpacing
+        ? props.navBarstyling.global.letterSpacing
+        : '1.25px',
+    fontWeight:
+      props.navBarstyling && props.navBarstyling.global.fontWeight
+        ? props.navBarstyling.global.fontWeight
+        : '800',
     '&:hover, &:focus': {
       borderRadius: '0',
     },
   }),
   buttonRootNoRightPadding: (props) => ({
-    padding: props.navBarstyling && (props.navBarstyling.cart.padding || props.navBarstyling.global.padding || '9px 20px 0px 20px'),
+    padding:
+      props.navBarstyling &&
+      (props.navBarstyling.cart.padding ||
+        props.navBarstyling.global.padding ||
+        '9px 20px 0px 20px'),
     border: '0',
     cursor: 'pointer',
     margin: '0',
@@ -93,9 +106,18 @@ const styles = () => ({
   }),
   cartLabelText: (props) => ({
     textDecoration: 'none',
-    color: props.navBarstyling && props.navBarstyling.global.fontColor ? props.navBarstyling.global.fontColor : '#FFFFFF',
-    fontFamily: props.navBarstyling && props.navBarstyling.global.fontFamily ? props.navBarstyling.global.fontFamily : 'Nunito',
-    textTransform: props.navBarstyling && props.navBarstyling.global.textTransform ? props.navBarstyling.global.textTransform : 'UPPERCASE',
+    color:
+      props.navBarstyling && props.navBarstyling.global.fontColor
+        ? props.navBarstyling.global.fontColor
+        : '#FFFFFF',
+    fontFamily:
+      props.navBarstyling && props.navBarstyling.global.fontFamily
+        ? props.navBarstyling.global.fontFamily
+        : 'Nunito',
+    textTransform:
+      props.navBarstyling && props.navBarstyling.global.textTransform
+        ? props.navBarstyling.global.textTransform
+        : 'UPPERCASE',
     fontSize: '13px',
   }),
   badge: {
@@ -104,8 +126,13 @@ const styles = () => ({
     verticalAlign: 'middle',
   },
   cartIcon: (props) => ({
-    width: "33px",
-    height: props.navBarstyling && props.navBarstyling.cart && props.navBarstyling.cart.iconSize ? props.navBarstyling.cart.iconSize : '27px',
+    width: '33px',
+    height:
+      props.navBarstyling &&
+      props.navBarstyling.cart &&
+      props.navBarstyling.cart.iconSize
+        ? props.navBarstyling.cart.iconSize
+        : '27px',
     margin: '0px 0px 0px 6px',
   }),
 });
