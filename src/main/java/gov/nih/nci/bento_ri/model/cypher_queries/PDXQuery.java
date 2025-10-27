8b8f79dc68cb4723c5dc5f30bcb3fd493b0aa1e2
wfy1997 <60631776+wfy1997@users.noreply.github.com>
2025-10-27 14:34:51 -0400

    Update PDXQuery.java

--- src/main/java/gov/nih/nci/bento_ri/model/cypher_queries/PDXQuery.java
+++ src/main/java/gov/nih/nci/bento_ri/model/cypher_queries/PDXQuery.java
@@ -2,18 +2,16 @@ package gov.nih.nci.bento_ri.model.cypher_queries;
 
 public class PDXQuery {
     public static final String PDX_QUERY = """
-        MATCH (p:pdx)-[:from_sample]->(:sample)-[:of_participant]->(:participant)-[:of_study]->(s:study {phs_accession: $phs_accession})
+        MATCH (p:pdx)-[:from_sample]->(samp:sample)-[:of_participant]->(:participant)-[:of_study]->(s:study {phs_accession: $phs_accession})
         WHERE
             $pdx_ids = [] OR p.pdx_id IN $pdx_ids
-        WITH p, {phs_accession: s.phs_accession} AS output
-        OPTIONAL MATCH (x)-->(samp:sample)
-        WITH p, samp, output
+        with p, samp, s
         WHERE
             $sample_ids = [] OR samp.sample_id IN $sample_ids
-        WITH p, apoc_replacement_poc.merge(output, {
+        with apoc_replacement_poc.merge({
+            phs_accession: s.phs_accession,
             sample_id: samp.sample_id
-        }) AS output
-        WITH apoc_replacement_poc.merge(output, p {.*}) AS output
+        }, p {.*}) AS output
         RETURN output
         ORDER BY output.pdx_id ASC
     """;
