06b77c92bcf4466aef6a2ff6bf82e6c261e38875
wfy1997 <60631776+wfy1997@users.noreply.github.com>
2025-10-28 10:42:19 -0400

    Update PDXQuery.java

--- src/main/java/gov/nih/nci/bento_ri/model/cypher_queries/PDXQuery.java
+++ src/main/java/gov/nih/nci/bento_ri/model/cypher_queries/PDXQuery.java
@@ -4,15 +4,12 @@ public class PDXQuery {
     public static final String PDX_QUERY = """
         MATCH (p:pdx)-[:from_sample]->(samp:sample)-[:of_participant]->(:participant)-[:of_study]->(s:study {phs_accession: $phs_accession})
         WHERE
-            $pdx_ids = [] OR p.pdx_id IN $pdx_ids
-        WITH p, samp, s
-        WHERE
-            $sample_ids = [] OR samp.sample_id IN $sample_ids
-        WITH apoc_replacement_poc.merge({
+            ($pdx_ids = [] OR p.pdx_id IN $pdx_ids) AND
+            ($sample_ids = [] OR samp.sample_id IN $sample_ids)
+        RETURN
+            p {.*,
             phs_accession: s.phs_accession,
-            sample_id: samp.sample_id
-        }, p {.*}) AS output
-        RETURN output
+            sample_id: samp.sample_id} AS output
         ORDER BY output.pdx_id ASC
     """;
 
