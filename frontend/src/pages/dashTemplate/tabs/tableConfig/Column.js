58d4b53e74e6070f0a7ef1be1b8921f07093d9fa
Alejandro-Vega <vegaalejandro15@gmail.com>
2026-03-02 16:11:48 -0500

    support linking to external using another field

--- frontend/src/pages/dashTemplate/tabs/tableConfig/Column.js
+++ frontend/src/pages/dashTemplate/tabs/tableConfig/Column.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import { Link, Typography } from '@material-ui/core';
-import { cellTypes, headerTypes } from '@bento-core/table';
-import DocumentDownloadView from '../../../../components/DocumentDownload/DocumentDownloadView';
+import React from "react";
+import { Link, Typography } from "@material-ui/core";
+import { cellTypes, headerTypes } from "@bento-core/table";
+import { externalLinkIcon } from "../../../../bento/dashboardTabData";
+import DocumentDownloadView from "../../../../components/DocumentDownload/DocumentDownloadView";
 
 export const CustomCellView = (props) => {
   const {
-    downloadDocument, documentDownloadProps,
-    displayEmpty, dataField, label,
+    downloadDocument,
+    documentDownloadProps,
+    displayEmpty,
+    dataField,
+    label,
+    externalLinkAttr,
   } = props;
   if (downloadDocument) {
     return (
@@ -20,8 +25,34 @@ export const CustomCellView = (props) => {
         requiredACLs={props[dataField]}
       />
     );
+  } else if (externalLinkAttr && externalLinkAttr.urlField) {
+    const url = props[externalLinkAttr.urlField];
+    const displayValue = props[dataField];
+    if (url) {
+      return (
+        <Link href={url} target="_blank" rel="noopener noreferrer">
+          <Typography>
+            {displayValue}
+            <img
+              src={externalLinkIcon.src}
+              alt={externalLinkIcon.alt}
+              style={{
+                width: "1em",
+                margin: "0px 0px 2px 2px",
+                verticalAlign: "sub",
+              }}
+            />
+          </Typography>
+        </Link>
+      );
+    }
+    return <Typography>{displayValue || ""}</Typography>;
   } else if (typeof displayEmpty === "boolean") {
-    return (<Typography>{displayEmpty || props[dataField] ? props[dataField] : ""}</Typography>);
+    return (
+      <Typography>
+        {displayEmpty || props[dataField] ? props[dataField] : ""}
+      </Typography>
+    );
   } else if (Array.isArray(label)) {
     if (props.linkAttr) {
       const { rootPath } = props.linkAttr;
@@ -29,37 +60,43 @@ export const CustomCellView = (props) => {
         <Typography>
           {label.map((item, idx) => {
             return (
-              <Link href={`#${rootPath}/`.concat(item)} className={cellTypes.LINK}>
-                <Typography key={idx}>{item}{idx !== label.length - 1 && ", "}</Typography>
+              <Link
+                href={`#${rootPath}/`.concat(item)}
+                className={cellTypes.LINK}
+              >
+                <Typography key={idx}>
+                  {item}
+                  {idx !== label.length - 1 && ", "}
+                </Typography>
               </Link>
             );
           })}
         </Typography>
       );
     }
-    
-    if (label.length > 5){
-      return (<Typography>{label.slice(0,5).join(", ") + ", ..."}</Typography>);
+
+    if (label.length > 5) {
+      return <Typography>{label.slice(0, 5).join(", ") + ", ..."}</Typography>;
     }
-    return (<Typography>{label.join(", ")}</Typography>);
+    return <Typography>{label.join(", ")}</Typography>;
   }
 
   // other custom elem
-  return (<></>);
+  return <></>;
 };
 
-export const CustomHeaderCellView = () => (<></>);
+export const CustomHeaderCellView = () => <></>;
 
 /**
-* set column configuration
-* @param {*} columns
-* @returns config columns
-*/
+ * set column configuration
+ * @param {*} columns
+ * @returns config columns
+ */
 export const configColumn = (columns) => {
   /**
-  * display columns as configuration
-  * set custom cell render for column
-  */
+   * display columns as configuration
+   * set custom cell render for column
+   */
   const displayColumns = columns;
   const displayCustomView = [...displayColumns].map((column) => {
     if (column.cellType === cellTypes.CUSTOM_ELEM) {
@@ -72,8 +109,8 @@ export const configColumn = (columns) => {
   });
 
   /**
-  * custom header view configuration
-  */
+   * custom header view configuration
+   */
   const displayCustomHeader = [...displayCustomView].map((column) => {
     if (column.headerType === headerTypes.CUSTOM_ELEM) {
       return {
@@ -85,3 +122,4 @@ export const configColumn = (columns) => {
   });
   return displayCustomHeader;
 };
+
