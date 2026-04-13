7887b1794e71174d477a3d20f20784b3d4bd2bf9
wfy1997 <60631776+wfy1997@users.noreply.github.com>
2026-04-13 14:01:10 -0400

    Update ProtocolQuery.java

--- backend/src/main/java/gov/nih/nci/bento_ri/model/cypher_queries/ProtocolQuery.java
+++ backend/src/main/java/gov/nih/nci/bento_ri/model/cypher_queries/ProtocolQuery.java
@@ -2,7 +2,7 @@ package gov.nih.nci.bento_ri.model.cypher_queries;
 
 public class ProtocolQuery {
     public static final String PROTOCOLS_QUERY = """
-        MATCH (prot:Protocol)-[*]->(:sample)-->(s:study {phs_accession: $phs_accession})
+        MATCH (prot:Protocol)-->(s:study {phs_accession: $phs_accession})
         WHERE
             $protocol_ids = [] OR prot.protocol_pk_id IN $protocol_ids
         WITH DISTINCT prot, s
@@ -17,7 +17,7 @@ public class ProtocolQuery {
     """;
 
     public static final String PROTOCOLS_COUNT_QUERY = """
-        MATCH (prot:Protocol)-[*]->(:sample)-->(s:study {phs_accession: $phs_accession})
+        MATCH (prot:Protocol)-->(s:study {phs_accession: $phs_accession})
         RETURN COUNT(DISTINCT prot) AS count
     """;
 }
